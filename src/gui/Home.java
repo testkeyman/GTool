fdd0f6003d8f050e586deb4c344da7e7af093fa0
Find <Findxiaoxun@gmail.com>
2014-07-17 14:09:11 +0800

    version 0.3 Fixing~

--- src/gui/Home.java
+++ src/gui/Home.java
@@ -1,15 +1,16 @@
 package gui;
 
-import java.awt.Button;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
@@ -21,57 +22,74 @@ public class Home extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = -4731380524981415485L;
-	//panels
+	final JLabel note1=new JLabel("Please choose the file or directory to transmit");
 	JTabbedPane pane=new JTabbedPane();
 	JPanel fileTransmit=new JPanel();
-	JPanel fileTransUp=new JPanel();
-	JPanel fileTransDown=new JPanel();
 	
-	JTextArea consoleText=new JTextArea("确定水平滚动条何时显示在滚动窗格上。选项有：ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED ：需要时出现ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEV从不出现ScrollPaneConsta：nts.HORIZONTAL_SCROLLBAR_ALWAYS ：总是出现垂直滚动paneAdds");
+	JTextArea consoleText=new JTextArea("");
+//	JTextArea path1;
+	JLabel path1;
 	JScrollPane console=new JScrollPane();
 	JFileChooser fileChooser=new JFileChooser();
-	Button choose=new Button("Choose");
+	JButton choose=new JButton("Choose");
+	JButton start=new JButton("start");
 	Dimension   screensize   =   Toolkit.getDefaultToolkit().getScreenSize();   
 	Dimension   framesize; 
+	private final JLabel note2 = new JLabel("Your file or directory will be copyed to ");
+	private final JLabel note3=new JLabel("/sdcard/Gtool/Your file or directory name");
 	public Home() {
-		// TODO Auto-generated constructor stub
 		super("Genymotion Tool");
 		setSize(400,500);
-		setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
 		
 		filePanelAdd();
 		paneAdds();
 		consoleSetting();
-		add(pane);
+		getContentPane().add(pane);
 		pane.setLocation(0, 0);
-		add(console);
-		console.setLocation(0, getSize().height/2);
+		getContentPane().add(console);
+//		console.setLocation(0, getSize().height/2);
 		moveToScreenCenter();
 		setVisible(true);
 	}
 	void paneAdds(){
 		
-//		pane.setBounds(0, 0, getSize().width, getSize().height/2);
 		pane.addTab("File Transmit", fileTransmit);
 	}
-	/**FileTransmitPanel adds and sets*/
+	/**FileTransmitPanel adds and settings*/
 	void filePanelAdd(){
 		fileTransmit.setLayout(new BoxLayout(fileTransmit,BoxLayout.Y_AXIS ));
+		
+		note1.setFont(new Font(null, Font.PLAIN, 16));
+		path1=new JLabel("You choose null ");
+		path1.setFont(new Font(null, Font.BOLD, 14));
+		choose.setFont(new Font(null, Font.BOLD, 14));
+		start.setFont(new Font(null, Font.BOLD, 14));
+//		path1.setLineWrap(true);
+//		path1.setRows(3);
+		note2.setFont(new Font(null, Font.PLAIN, 16));
+		note3.setFont(new Font(null, Font.PLAIN, 16));
 		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		choose.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				int returnVal = fileChooser.showOpenDialog(getParent());
 				if(returnVal == JFileChooser.APPROVE_OPTION) {
 					System.out.println ( fileChooser.getSelectedFile().getPath() );
+					path1.setText(fileChooser.getSelectedFile().getPath());
+					System.out.println(fileChooser.getSelectedFile().isDirectory());
 				}
 				
 			}
 		});
+		fileTransmit.add(note1);
+		fileTransmit.add(path1);
 		fileTransmit.add(choose);
 		
+		fileTransmit.add(note2);
+		fileTransmit.add(note3);
+		fileTransmit.add(start);
 	}
 	void consoleSetting(){
-//		console.setBounds(0, getSize().height/2, getSize().width, getSize().height/2);
 		consoleText.setLineWrap(true);
 		console.setViewportView(consoleText);
 		console.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
